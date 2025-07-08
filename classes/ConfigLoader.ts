d31cc1f6f1ae3d3649ed3ee28ea232114ab93f95
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2025-07-08 20:32:33 +0200

    Added logger to config loader

--- classes/ConfigLoader.ts
+++ classes/ConfigLoader.ts
@@ -1,8 +1,15 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { Logger } from './Logger.ts';
 
 class ConfigLoader {
+    private logger: Logger;
+    private port: number;
+    private ssl: boolean;
+    private config: any;
+
     constructor() {
+        this.logger = new Logger();
         let cfg;
         const configPath = path.resolve('./config.json');
         const configTemplatePath = path.resolve('./config_template.json');
@@ -11,11 +18,11 @@ class ConfigLoader {
             if (fs.existsSync(configPath)) {
                 cfg = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
             } else {
-                console.warn("config.json not found. Falling back to config_template.json.");
+                this.logger.warn("config.json not found. Falling back to config_template.json.");
                 cfg = JSON.parse(fs.readFileSync(configTemplatePath, 'utf-8'));
             }
         } catch (e) {
-            console.error("Error reading configuration files:", e);
+            this.logger.error("Error reading configuration files:", e);
             throw new Error("Failed to load configuration.");
         }
 
